0a4ed430da512fb678234bf721a4e8511361aa7a
Svetlana Ignatova <Svetlana_ignatova@epam.com>
2024-11-06 18:41:27 +0100

    task week2-v6

--- weeks/week02/homework/week2.py
+++ weeks/week02/homework/week2.py
@@ -1,54 +1,54 @@
 ################    task1    #####################
 #buble sorting Asc
-# def bubble_sort_ascending(arr):
-#     for i in range(len(arr)):
-#         for j in range(0, len(arr)-i-1):
-#             if arr[j] > arr[j + 1]:
-#                 arr[j], arr[j + 1] = arr[j + 1], arr[j]
-#     return arr
-# msort = bubble_sort_ascending([1,3,2,4])
-# print(msort)
-#
+def bubble_sort_ascending(arr):
+    for i in range(len(arr)):
+        for j in range(0, len(arr)-i-1):
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+    return arr
+msort = bubble_sort_ascending([1,3,2,4])
+print(msort)
+
 # ##buble sorting Desc
-# def bubble_sort_descending(arr):
-#     for i in range(len(arr)):
-#         for j in range(0, len(arr)-i-1):
-#             if arr[j] < arr[j + 1]:
-#                 arr[j], arr[j + 1] = arr[j + 1], arr[j]
-#     return arr
-# msort = bubble_sort_descending([1,3,2,4])
-# print(msort)
-#
-#
+def bubble_sort_descending(arr):
+    for i in range(len(arr)):
+        for j in range(0, len(arr)-i-1):
+            if arr[j] < arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+    return arr
+msort = bubble_sort_descending([1,3,2,4])
+print(msort)
+
+
 # ##buble sorting pause
-# def bubble_sort_pause(arr):
-#     for i in range(len(arr)):
-#         is_sorted = True
-#         for j in range(0, len(arr)-i-1):
-#             if arr[j] > arr[j + 1]:
-#                 arr[j], arr[j + 1] = arr[j + 1], arr[j]
-#                 is_sorted = False
-#         if is_sorted:
-#             break
-#     return arr
-# msort = bubble_sort_pause([1,3,2,4])
-# print(msort)
-#
+def bubble_sort_pause(arr):
+    for i in range(len(arr)):
+        is_sorted = True
+        for j in range(0, len(arr)-i-1):
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+                is_sorted = False
+        if is_sorted:
+            break
+    return arr
+msort = bubble_sort_pause([1,3,2,4])
+print(msort)
+
 #
 # ################   task2    #####################
-# def square(n, cache={}):
-#     if n in cache:
-#         print("taken from cache:")
-#         return cache[n]
-#     else:
-#         print("Calculating result:")
-#         result = n * n
-#         cache[n] = result
-#         return result
-# print(square(4))
-# print(square(4))
-# print(square(5))
-# print(square(5))
+def square(n, cache={}):
+    if n in cache:
+        print("taken from cache:")
+        return cache[n]
+    else:
+        print("Calculating result:")
+        result = n * n
+        cache[n] = result
+        return result
+print(square(4))
+print(square(4))
+print(square(5))
+print(square(5))
 
 ################   task3    #####################
 test_case1 = ["open browser", "navigate to page", "fill form"]
